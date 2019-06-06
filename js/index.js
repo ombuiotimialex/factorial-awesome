e9a60ac9c08365d093103e65ba4a9efccf3487ef
ombuiotimialex <ombuiotimialex@gmail.com>
2019-06-06 13:31:54 +0300

    Initial commit
    Added html, js and css files to project

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function product() {
+    a = Number.parseInt(document.getElementById("a").value)
+    b = 1;
+    if (a == NaN) {
+        alert("Invalid input");
+    } else {
+        for (i = a; i > 0; i--) {
+            b = b * i;
+        }
+
+        document.getElementById("solution").value = a + "! =" + b;
+
+    }
+}
