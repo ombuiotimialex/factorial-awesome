d7c048abe43f5def61fdaada1e922c1566d1994b
ombuiotimialex <ombuiotimialex@gmail.com>
2019-06-07 11:18:05 +0300

    Update index.js
    read only numers and not any other data types

--- js/index.js
+++ js/index.js
@@ -1,21 +1,18 @@
 function factorial() {
-    let a = Number.parseInt(document.getElementById("a").value)
+    let a = Number.parseFloat(document.getElementById("a").value);
     var b = 1;
 
-    if (a < 1) {
     if (a < 1 || a === Number.NaN || (a - Math.round(a) != 0)) {
         alert("Invalid input");
-    } else if(typeof a==="string") {
-        alert("invalid input")
-    }else{
+    } else {
         for (var i = a; i > 0; i--) {
             b = b * i;
-        }
 
+        }
         let solution = document.getElementById("solution");
 
-        solution.style.visibility = "visible";
+        solution.style.visibility = "visible"
 
         document.getElementById("solution").innerText = a + "! =" + b;
     }
-}
+};
